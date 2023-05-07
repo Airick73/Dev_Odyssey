27907f5595b18a4d9f9e08579b15ee10ba16b7c2
Eric Kemmer <eric199900@gmail.com>
2023-05-07 09:54:26 -0400

    android configuraiton + adding to folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,40 @@
-import 'dart:math';
-import 'dart:ui';
-
 import 'package:dev_odyssey/shared/loading.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import "package:flutter/material.dart";
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text("place holder")),
+        body: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Error'));
+            } else if (snapshot.connectionState == ConnectionState.done) {
+              return Center(child: Text('App'));
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
