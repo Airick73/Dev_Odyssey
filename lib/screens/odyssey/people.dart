43e813fe3774fe5630267398726b34371a622868
Eric Kemmer <eric199900@gmail.com>
2023-08-18 10:02:50 -0400

    Adding rough draft of people and resources screen for a odyssey.

--- lib/screens/odyssey/people.dart
+++ lib/screens/odyssey/people.dart
@@ -1,16 +1,67 @@
+import 'package:dev_odyssey/models/odyssey_model.dart';
+import 'package:dev_odyssey/models/person_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class PeopleScreen extends StatelessWidget {
   const PeopleScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final odyssey = Provider.of<OdysseyModel>(context);
+    final people = odyssey.people.values.toList();
+
+    // Determine the number of columns based on screen width
+    final width = MediaQuery.of(context).size.width;
+    final columns = width ~/ 150; // 150 is the approximate width of each item
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Welcome to the people screen"),
       ),
-      body: const Center(
-        child: Text("People Screen"),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(8.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: columns,
+          childAspectRatio: 0.8,
+        ),
+        itemCount: people.length,
+        itemBuilder: (context, index) {
+          final person = people[index];
+          return _buildPersonCard(person);
+        },
+      ),
+    );
+  }
+
+  Widget _buildPersonCard(PersonModel person) {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundImage: person.picture.isNotEmpty
+                  ? NetworkImage(person.picture)
+                  : null, //const AssetImage('assets/default_image.png'), // Default image
+              radius: 40,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              person.name,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/odyssey/resources.dart
+++ lib/screens/odyssey/resources.dart
@@ -1,16 +1,43 @@
+import 'package:dev_odyssey/models/odyssey_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ResourcesScreen extends StatelessWidget {
-  const ResourcesScreen({super.key});
+  const ResourcesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Determine the number of columns based on screen width
+    final width = MediaQuery.of(context).size.width;
+    final columns = width > 600
+        ? 3
+        : 2; // Example: 3 columns for wide screens, 2 for narrow
+    final odyssey = Provider.of<OdysseyModel>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Welcome to the resouces screen"),
+        title: const Text("Welcome to the resources screen"),
       ),
-      body: const Center(
-        child: Text("Resources screen"),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: columns,
+          childAspectRatio: 3 / 2, // Adjust as needed
+        ),
+        itemCount: odyssey.resources.length,
+        itemBuilder: (context, index) {
+          final resource = odyssey.resources.values.elementAt(index);
+          return Card(
+            child: Column(
+              children: [
+                if (resource.picture != null)
+                  Image.network(
+                      resource.picture!), // Display picture if available
+                Text(resource.note ?? 'No note available'),
+                Text(resource.link ?? 'No link available'),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
